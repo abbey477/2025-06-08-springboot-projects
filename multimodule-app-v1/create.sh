14a4b971bbcb2235677bb8dab01ed65c45e98e81
Abi Okusolubo <abbey477@gmail.com>
2025-06-08 11:00:39 -0500

    firs commit

---
+++ multimodule-app-v1/create.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# service-module/src/main/java/com/example/service/UserService.java
+mkdir service-module/src/main/java/com/example/service/
+touch service-module/src/main/java/com/example/service/UserService.java
