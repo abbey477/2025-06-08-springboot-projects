482c8c575bc7e66256c0dd8efca9f1fca754b8db
Abi Okusolubo <abbey477@gmail.com>
2025-06-13 22:58:31 -0500

    added demo on property mapping

---
+++ tutorial-v1/demo-app-property-2/src/main/java/com/example/demoappproperty/DemoAppPropertyApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoappproperty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppPropertyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAppPropertyApplication.class, args);
+    }
+}
